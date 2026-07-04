67f83f09bccc4669abe164feebf68efc974383bc
ArnavShelke04 <arnavshelke2006@gmail.com>
2026-07-05 01:10:47 +0530

    tried making poolschema

--- backend/models/poolSchema.js
+++ backend/models/poolSchema.js
@@ -1,2 +1,19 @@
-import mongoose, { Schema } from "mongoose";
-mongoose schema
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const poolSchema = new Schema({
+    pool_id : String,
+  subscription : {
+    name : String,
+    monthly_cost : String,
+    category :
+  },
+  creator :{
+    creator_id : String,
+    creator_accname : String,
+    avatarUrl : String
+  }
+},{
+ timestamps :true
+});
+export const Pool = mongoose.model('Pool',poolSchema)
